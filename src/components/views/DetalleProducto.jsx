33f431576ce0155f19d771beed40a9844e8d88ee
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-11-29 13:07:06 -0300

    agregue cambios en el maquetado de la pagina de detalles producto

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -1,49 +1,52 @@
 import { useEffect, useState } from "react";
-import { Card,Col,Badge,Row } from "react-bootstrap";
+import { Card, Col, Badge, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { obtenerProductoApi } from "../helpers/queries";
 import Swal from "sweetalert2";
 
 const DetalleProducto = () => {
-  const {id}=useParams();
-  const [producto,setProducto]=useState({});
-  useEffect(()=>{
-    obtenerProductoApi(id).then((respuesta)=>{
-      console.log(respuesta)
-      if (respuesta.status ===200){
+  const { id } = useParams();
+  const [producto, setProducto] = useState({});
+  useEffect(() => {
+    obtenerProductoApi(id).then((respuesta) => {
+      console.log(respuesta);
+      if (respuesta.status === 200) {
         setProducto(respuesta.dato);
-      }else{
+      } else {
         Swal.fire(
           "Ocurrio un error",
           "Intente este paso en unos minutos",
           "error"
         );
       }
-    })
-  },[]);
-    return (
-        <Card className="container my-5 mainSection py-5">
-        <Row className="w-75">
-          <Col md={5}>
-            <img src={producto.imagen} alt="producto" className="w-100" />
-          </Col>
-          <Col md={5} className="py-3">
-            <h3>{producto.nombreProducto}</h3>
-            <hr />
-            <Badge bg="success">{producto.categoria}</Badge>
-            <div>
+    });
+  }, []);
+  return (
+    <Card className="container my-5 mainSection py-5 fuente">
+      <Row className="w-75">
+        <Col md={5}>
+          <img src={producto.imagen} alt="producto" className="w-100" />
+        </Col>
+        <Col md={5} className="py-3">
+          <h2 className="fs-1">{producto.nombreProducto}</h2>
+          <hr />
+          <Badge bg="success">{producto.categoria}</Badge>
+          <div>
             <p className="mt-3">
               <b>Precio: ${producto.precio}</b>
             </p>
-            </div>
-            <div>
-                <h4>Descripcion</h4>
-                <p>{producto.detalle}</p>
-            </div>
-          </Col>
-        </Row>
-      </Card>
-    );
+          </div>
+          <div>
+            <h3>
+              <b>Descripcion</b>
+            </h3>
+
+            <p>{producto.detalle}</p>
+          </div>
+        </Col>
+      </Row>
+    </Card>
+  );
 };
 
 export default DetalleProducto;
